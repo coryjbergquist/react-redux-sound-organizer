296b0350e091391ce72b3e5b25f471a1c9fec366
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-01 09:53:37 -0700

    format SoundsListContainer

--- src/containers/SoundsListContainer.js
+++ src/containers/SoundsListContainer.js
@@ -4,14 +4,12 @@ import  SoundsList  from '../components/SoundsList';
 
 class SoundsListContainer extends React.Component {
 
+  render() {
+    return <SoundsList sounds={this.props.sounds} />;
+  }
 
-    render() {
-      return <SoundsList sounds={this.props.sounds} />;
-    }
 }
 
-
 const mapStateToProps = state => ({sounds: state.sounds})
-
 
 export default connect(mapStateToProps)(SoundsListContainer);
