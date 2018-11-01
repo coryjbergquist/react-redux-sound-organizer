b73d51114a48c56796c8f00efb1db1a2826b541e
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-01 08:46:04 -0700

    change container name to SoundsListContainer

--- src/components/SoundsList.js
+++ src/components/SoundsList.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SoundsList = ({ sounds }) => {
+  debugger
 
   const renderSounds = sounds.map(sound =>
     <div key={sound}>{sound}</div>
     //<Link style={{ marginRight: '12px' }} key={sound.id} to={`/pets/${pet.id}`}>{pet.name}</Link>
+    //make link to each sound to show description etc.
   );
 
   return (
