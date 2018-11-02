f9cc6872b300785176cb2fe4d452f5318dd8f9eb
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-01 17:57:12 -0700

    add props to SoundsList for delete button

--- src/components/SoundsList.js
+++ src/components/SoundsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SoundsList = ({ sounds, countDown }) => {
+const SoundsList = ({ sounds, countDown, props }) => {
 
   const renderSounds = sounds.sound.map(sound =>
     <li>{sound}</li>
