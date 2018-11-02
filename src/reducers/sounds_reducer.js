5ee61eb45d572b89a2ebe4db43b2b5a780968e6b
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-02 10:54:11 -0700

    add ADD_COMMENT to sounds_reducer

--- src/reducers/sounds_reducer.js
+++ src/reducers/sounds_reducer.js
@@ -3,25 +3,26 @@ export const cuidFn = cuid;
 
 export default function soundsReducer(state= {
   sounds: [],
-  soundComments: [],
+  comments: [],
 }, action) {
   switch ( action.type ) {
-      //ADD SOUND
+      //ADD_SOUND
       //make action.sound include the new sound object
+      //eventually includes sound file
     case 'ADD_SOUND':
       const sound = {name: action.payload.name, description: action.payload.description, id: cuidFn()};
-     //return { sound: state.sound.concat(sound.sound) };
       return { ...state, sounds: [...state.sounds, sound]}
-//-------------------------------------------------------------------------
-      //REMOVE SOUND by filtering all sounds and returning everything
-      //except the one that matches
-      //will return a new state that doesn't include the matching sound.id
-      //must assign an id to all sounds, and create soundIndex?
-      //action.id must include the sound id and maybe other stuff?
-    case 'DELETE_SOUND':
 
+    case 'DELETE_SOUND':
       const sounds = state.sounds.filter(sound => sound.id !== action.payload);
       return {...state, sounds}
+
+      //connect comment to sound through soundId
+    case 'ADD_COMMENT':
+      const comment = { text: action.comment.text, soundId: action.comment.soundId, id: cuidFn() };
+      return { ...state,
+        comments: [...state.comments, comment]
+    }
 
       //creating loading bar to show when loading sounds until complete
     case 'LOADING_SOUNDS':
