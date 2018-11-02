089b5a5037eaa9b392ad8fc02fb8b3b4622d60b8
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-02 11:23:40 -0700

    add AddComment.js to components dir

---
+++ src/components/AddComment.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class AddComment extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.addComment({text: this.state.text, soundId: this.props.soundId });
+    this.setState({
+      text: '',
+    });
+  }
+
+  render() {
+    return (
+     <div>
+        <form onSubmit={this.handleOnSubmit} >
+          <label>Add Comment</label>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleOnChange} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+};
+
+export default AddComment;
