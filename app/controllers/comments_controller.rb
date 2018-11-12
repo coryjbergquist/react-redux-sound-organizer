77558d45424b8bfdea9eb86edec41d23c86948b3
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-12 07:35:54 -0700

    upcase comment in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,30 +3,30 @@ require 'pry'
 class CommentsController < ApplicationController
 
   def index
-    @comments = comment.all
+    @comments = Comment.all
     render json: @comments, status: 200
   end
 
   def show
-    @comment = comment.find(params[:id])
+    @comment = Comment.find(params[:id])
     render json: @comment, status: 200
   end
 
   def create
-    @comment = comment.create(comment_params)
+    @comment = Comment.create(comment_params)
     render json: @comment, status: 200
   end
 
   def update
-    @comment = comment.find(params[:id])
+    @comment = Comment.find(params[:id])
     @comment.update(comment_params)
     render json: @comment, status: 200
   end
 
   def destroy
-    @comment = comment.find(params[:id])
+    @comment = Comment.find(params[:id])
     @comment.delete
-    @comments = comment.all
+    @comments = Comment.all
     render json: @comments, status: 200
   end
 
