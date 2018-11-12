3276dcc12b256c72b3922600dcd564eb20e40fef
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-12 06:27:53 -0700

    add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # root 'sounds#index'
 
   resources :sounds
-  #get 'sounds', to: 'sounds#index'
+
+  resources :comments
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
