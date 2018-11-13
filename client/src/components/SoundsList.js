f30fc8eddbee5b2396e5f0baf5b08902f0692b0d
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-13 15:43:32 -0700

    move comment in SoundList component

--- client/src/components/SoundsList.js
+++ client/src/components/SoundsList.js
@@ -8,23 +8,10 @@ class SoundsList extends React.Component { //= (props) => {
     super(props)
   }
 
-
+  //adding a Sounds component for each sound props
   renderSounds = this.props.sounds.sounds.map((sound) =>
-    <Sound sound={sound}/> //adding a Sounds component for each sound props
+    <Sound sound={sound}/>
   )
-
-// renderSounds = this.props.sounds.sounds.map((sound) =>
-// //link to each sound to show description etc.
-//       <li key={sound.id}>
-//       <Link key={sound.id} to={`/sounds/${sound.id}`}>{sound.name}</Link>
-//       <p>
-//         <button onClick={() => this.props.delete(sound.id)}>DELETE </button>
-//         <button onClick={this.onClick}>LIKE </button>
-//
-//       </p>
-//     </li>
-//
-//   );
 
   render(){
 
