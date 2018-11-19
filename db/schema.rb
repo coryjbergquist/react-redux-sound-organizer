e3a68991c6398de9845a30a4dee3fdcaa7e2f081
coryjbergquist <cory.j.bergquist@gmail.com>
2018-11-19 12:55:37 -0700

    switch database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_12_162007) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.string "sound_id"
