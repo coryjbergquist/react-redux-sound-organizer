f311105633116c21681eaf6bfd20db60c02fee36
coryjbergquist <cory.j.bergquist@gmail.com>
2018-12-11 16:59:53 -0700

    remove unneccessary comments in sounds_reducer

--- client/src/reducers/sounds_reducer.js
+++ client/src/reducers/sounds_reducer.js
@@ -7,45 +7,16 @@ export default function soundsReducer(state= {
   loading: false,
 }, action) {
   switch ( action.type ) {
-      //ADD_SOUND
-      //make action.sound include the new sound object
-      //eventually includes sound file
-
-    // case 'ADD_SOUND':
-    //   const sound = {name: action.payload.name, description: action.payload.description, id: cuidFn()};
-    //   return { ...state, sounds: [...state.sounds, sound]}
-
-    // case 'DELETE_SOUND':
-    //   const sounds = state.sounds.filter(sound => sound.id !== action.payload);
-    //   return {...state, sounds}
-
-      //connect comment to sound through soundId
-    // case 'ADD_COMMENT':
-    //   const comment = { text: action.payload.text, soundId: action.payload.soundId, id: cuidFn() };
-    //   return { ...state,
-    //     comments: [...state.comments, comment]
-    // }
-
-    // case 'DELETE_COMMENT':
-    //   const comments = state.comments.filter(comment => comment.id !== action.payload);
-    //   return {...state, comments}
-
+    
       //creating loading bar to show when loading sounds until complete
     case 'LOADING_SOUNDS':
-
       return Object.assign({}, state, {loading: true})
 
       //fetch the sounds
     case 'FETCH_SOUNDS':
-
-      //const apiSounds = action.payload.map(sound => sound)
-
         return { loading: false, ...state, sounds: action.payload}
 
-
-
     case 'ADD_SOUND_STARTED':
-
       console.log("started to add sound")
       return {
         ...state,
@@ -53,7 +24,6 @@ export default function soundsReducer(state= {
     };
 
     case 'ADD_SOUND_SUCCESS':
-
       console.log(`Successfully added sound! ${action.payload}`)
       return {
         loading: false, ...state, sounds: [...state.sounds, action.payload]
@@ -67,13 +37,12 @@ export default function soundsReducer(state= {
       };
 
     case 'DELETE_SOUND_START':
-
       return {
         ...state, loading: true,
       }
 
     case 'DELETE_SOUND_SUCCESS':
-    
+
       return {
         loading: false, ...state, sounds: action.payload
       }
