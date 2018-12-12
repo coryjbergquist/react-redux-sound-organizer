8641c57293db1b14bb48ef2e17a5a7ee7e0c02a9
coryjbergquist <cory.j.bergquist@gmail.com>
2018-12-11 17:00:50 -0700

    remove unneccessary imports from index reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import soundsReducer from './sounds_reducer'
-//import manageSounds from './manage_sounds'
 
 const rootReducer = combineReducers({
   sounds: soundsReducer
